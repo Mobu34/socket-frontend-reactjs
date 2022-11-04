3bd48d796869975e59d9891f364904cd1efb5e31
Lucas <lucas.tidas@wefight.co>
2022-11-04 17:04:51 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3001", { transports: ["websocket"] });
 
 function App() {
+  console.log({ socket });
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [responses, setResponses] = useState([]);
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("CONNECTED");
+      setIsConnected(socket.connected);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("DISCO");
+      setIsConnected(true);
+    });
+
+    socket.on("getChat", (res) => {
+      const newResponses = [...responses];
+      console.log(newResponses);
+      newResponses.push(res as never);
+      setResponses(newResponses);
+    });
+
+    return () => {
+      console.log("RETURN");
+      socket.off("connect");
+      socket.off("disconnect");
+    };
+  }, []);
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    socket.emit("events", event.target.message.value, (res: any) => {
+      const newResponses = [...responses];
+      newResponses.push(res as never);
+      setResponses(newResponses);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <form onSubmit={onSubmit}>
+          <input
+            title="Ecris moi ton message"
+            placeholder="ICI"
+            onChange={() => {}}
+            name="message"
+            type="text"
+          />
+          <button type="submit">Envoyer</button>
+        </form>
+        <p>SOCKET connected = {isConnected ? "YES" : "NO"}</p>
+        <p>ID = {socket?.id ? socket.id : undefined}</p>
+        <h1>My Chat</h1>
+        {responses.map((response, index) => {
+          return <p key={index}>FROM server : {response}</p>;
+        })}
+      </>
     </div>
   );
 }
